93852f4fc2b249ed873a27fce95f733ff93ac3fa
chisom precious <85879745+chisompreciousc007@users.noreply.github.com>
2021-06-24 17:46:56 +0100

    added build

---
+++ client/build/precache-manifest.8064c2a7982891c08a4a83273e927f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88658bd2019c2183ec5799415536035f",
+    "url": "/hash/index.html"
+  },
+  {
+    "revision": "467c20a6241e1edbbc79",
+    "url": "/hash/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "467c20a6241e1edbbc79",
+    "url": "/hash/static/js/2.6296434f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hash/static/js/2.6296434f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba56d20931040577477",
+    "url": "/hash/static/js/main.f22ad0f0.chunk.js"
+  },
+  {
+    "revision": "331c5c5182433ab73f13",
+    "url": "/hash/static/js/runtime-main.19f3849f.js"
+  }
+]);
